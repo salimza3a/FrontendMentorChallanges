2cac4d901b89de32da1cdd70c1b3ee99d7553aae
salim salimzada <salimsalimzada2002@gmail.com>
2022-08-30 09:50:15 +0400

    Fix code format

--- advice generator/advice.js
+++ advice generator/advice.js
@@ -1,43 +1,38 @@
-const adviceNumber = document.querySelector('.advice_number');
-const adviceText = document.querySelector('.card_text');
+const adviceNumber = document.querySelector(".advice_number");
+const adviceText = document.querySelector(".card_text");
 
-const btn = document.querySelector('.btn');
-
-
-
-
-
+const btn = document.querySelector(".btn");
 
 const ADVICE_API = "https://api.adviceslip.com/advice";
 
+fetch(ADVICE_API)
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    generateAdvice(data.slip);
+  });
+
 function generateRandomNum() {
-let randomValue = Math.floor(Math.random() * 100)
-getNewAdvice(randomValue)
-}
-fetch(ADVICE_API).then(res => {
-    return res.json()
-}).then (data => {
-    generateAdvice(data.slip)
-})
-
-async function getNewAdvice (value) {
-    const res = await fetch(`https://api.adviceslip.com/advice/${value}`);
-    const data = await res.json();
-   try{
-    generateAdvice(data.slip)
-   }
-   catch(err) {
-    console.log(err)
-   }
+  let randomValue = Math.floor(Math.random() * 100);
+  getNewAdvice(randomValue);
 }
 
-async function generateAdvice (arg) {
-    const response = await arg
+async function generateAdvice(arg) {
+  const response = await arg;
 
-    adviceNumber.innerText = `Advice #${response?.id}`
-    adviceText.innerText = response?.advice;
-   
-   
+  adviceNumber.innerText = `Advice #${response?.id}`;
+  adviceText.innerText = response?.advice;
 }
 
-btn.addEventListener('click', generateRandomNum)
+async function getNewAdvice(value) {
+  const res = await fetch(`https://api.adviceslip.com/advice/${value}`);
+  const data = await res.json();
+  try {
+    generateAdvice(data.slip);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+btn.addEventListener("click", generateRandomNum);
